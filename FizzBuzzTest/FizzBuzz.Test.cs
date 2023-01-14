a93d2db4a8301c8e5f0093465b248d6d5d7304d8
Francisco Arboleda <francisco.arboleda@dedalus.eu>
2023-01-14 10:37:20 +0100

    Fran001 Refactor Fizzbuff cases into a theory.

--- FizzBuzzTest/FizzBuzz.Test.cs
+++ FizzBuzzTest/FizzBuzz.Test.cs
@@ -15,51 +15,28 @@ namespace FizzBuzzTest
             _sut.FizzBuzz(1);
         }
 
-
         [Fact]
         public void FizzBuzzOfZeroMustThrowError()
         {
             Assert.Throws<ArgumentException>( () => _sut.FizzBuzz(0) );
         }
 
-
         [Fact]
-        public void FizzbuzzOf5MustBeBuzz()
-        {
-            Assert.Equal( "Buzz", _sut.FizzBuzz(5));
-        }
-
-        [Fact]
-        public void FizzbuzzOf3MustBeFizz()
-        {
-            Assert.Equal("Fizz", _sut.FizzBuzz(3));
-        }
-        
-        [Fact]
-        public void FizzbuzzOf15MustBeFizzBuzz()
-        {
-            Assert.Equal("FizzBuzz", _sut.FizzBuzz(15));            
-        }
-
-
-        [Fact]
-        public void FizzbuzzOf2MustBeStr2()
-        {
-            Assert.Equal("2", _sut.FizzBuzz(2));            
-        }
-
-
-        [Fact]
-        public void FizzbuzzOf100MustBeBuzz()
-        {
-            Assert.Equal("Buzz", _sut.FizzBuzz(100));
-        }
-
-
-        [Fact]
-        public void FizzbuzzOfAValueGratherThan100MustThrowError()
+        public void FizzBuzzOfA_ValueGratherThan100MustThrowError()
         {
           Assert.Throws<ArgumentException>( () => _sut.FizzBuzz(100 + 1 ) );
+        }
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(100, "Buzz")]
+        public void FizzBuzzOfA_SetOfValuesMatches(sbyte inputValue, string output)
+        {
+          Assert.Equal(output, _sut.FizzBuzz(inputValue));
         }
 
 
